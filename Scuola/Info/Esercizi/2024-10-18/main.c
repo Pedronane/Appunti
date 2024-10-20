7cbecf9a3b3e87b66a89536c712eee0f4db10584
Pedronane <march3sipietro@gmail.com>
2024-10-20 20:05:15 +0200

    Un po di roba

---
+++ Scuola/Info/Esercizi/2024-10-18/main.c
@@ -0,0 +1,17 @@
+/*Avere un file binario con i dati dei prodotti , devo fare una procedura che mi prepari un file csv in ordine crescente di codice per categoria*/
+
+/*
+INIZIO
+  apri file f in lettura
+  apri file p in scrittura
+  se file f non e aperto
+  allora
+    scrivi il file non esiste
+  altrimenti 
+    keggi il primo record dal flile f
+    mentre il file non e finito 
+      ricerca la posizione 
+      leggi record successivo
+    fciclo
+FINE
+*/
